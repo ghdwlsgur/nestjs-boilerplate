4509efaede10dfb48afb2231a59c6969479ab415
HongJinHyeok <redmax45@naver.com>
2022-03-01 21:09:10 +0900

    서비스 정의

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,36 @@
+import { CreateBoardDto } from './dto/create-board.dto';
+import { Board, BoardStatus } from './board.model';
+import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    // const title = createBoardDto.title;
+    // const description = createBoardDto.description;
+    const { title, description } = createBoardDto;
+
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+}
