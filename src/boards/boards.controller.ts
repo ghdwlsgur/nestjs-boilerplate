63d54db2739ffd5013fd0d186b8f87993f942487
HongJinHyeok <redmax45@naver.com>
2022-03-01 21:08:58 +0900

    컨트롤러 정의

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardsService } from './boards.service';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  // DTO 사용 전
+  // @Post('/')
+  // createBoard(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Board {
+  //   return this.boardsService.createBoard(title, description);
+  // }
+
+  // DTO 사용 후
+  @Post('/')
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+}
