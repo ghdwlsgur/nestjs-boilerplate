69517973940344e40c0f34a0d2d527f528fc9c67
HongJinHyeok <redmax45@naver.com>
2022-03-03 00:57:01 +0900

    로컬 해제(주석 처리)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -11,49 +11,65 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get('/')
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
-
-  // DTO 사용 전
-  // @Post('/')
-  // createBoard(
-  //   @Body('title') title: string,
-  //   @Body('description') description: string,
-  // ): Board {
-  //   return this.boardsService.createBoard(title, description);
-  // }
-
-  // DTO 사용 후
-  @Post('/')
+  @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardsService.createBoard(createBoardDto);
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
-
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string): void {
-    this.boardsService.deleteBoard(id);
-  }
-
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
 }
+// @Controller('boards')
+// export class BoardsController {
+//   constructor(private boardsService: BoardsService) {}
+
+//   @Get('/')
+//   getAllBoard(): Board[] {
+//     return this.boardsService.getAllBoards();
+//   }
+
+//   // DTO 사용 전
+//   // @Post('/')
+//   // createBoard(
+//   //   @Body('title') title: string,
+//   //   @Body('description') description: string,
+//   // ): Board {
+//   //   return this.boardsService.createBoard(title, description);
+//   // }
+
+//   // DTO 사용 후
+//   @Post('/')
+//   @UsePipes(ValidationPipe)
+//   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+//     return this.boardsService.createBoard(createBoardDto);
+//   }
+
+//   @Get('/:id')
+//   getBoardById(@Param('id') id: string): Board {
+//     return this.boardsService.getBoardById(id);
+//   }
+
+//   @Delete('/:id')
+//   deleteBoard(@Param('id') id: string): void {
+//     this.boardsService.deleteBoard(id);
+//   }
+
+//   @Patch('/:id/status')
+//   updateBoardStatus(
+//     @Param('id') id: string,
+//     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+//   ): Board {
+//     return this.boardsService.updateBoardStatus(id, status);
+//   }
+// }
