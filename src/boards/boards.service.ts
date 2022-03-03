7174097bc288837f19f0a08ea7a59920bb103f00
HongJinHyeok <redmax45@naver.com>
2022-03-03 22:04:57 +0900

    보드 컨트롤러 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -21,6 +21,23 @@ export class BoardsService {
   createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardRepository.createBoard(createBoardDto);
   }
+
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.boardRepository.delete(id);
+    if (result.affected === 0)
+      throw new NotFoundException(`Can't find Board with id: ${id}`);
+  }
+
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
+    board.status = status;
+    await this.boardRepository.save(board);
+    return board;
+  }
+
+  async getAllBoards(): Promise<Board[]> {
+    return this.boardRepository.find();
+  }
 }
 // @Injectable()
 // export class BoardsService {
